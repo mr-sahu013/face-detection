7f93a389ca6480452a5cd37eb0da5ebf8fbedb43
mr-sahu013 <moji915511@gmail.com>
2025-08-29 17:47:05 +0530

    adding function to detect face and show squear

--- face-detection.py
+++ face-detection.py
@@ -1,8 +1,19 @@
 import cv2
 
+face_cap = cv2.CascadeClassifier("C:/Users/login/anaconda3/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")
 video_cap = cv2.VideoCapture(0)
 while True:
     ret, video = video_cap.read()
+    col = cv2.cvtColor(video, cv2.COLOR_BGR2GRAY)
+    faces = face_cap.detectMultiScale(
+        col,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    for (x, y, w, h) in faces:
+        cv2.rectangle(video, (x, y), (x+w, y+h), (0, 255, 0), 2)    
     cv2.imshow("video", video)
     if cv2.waitKey(10) == ord('q'):
         break
